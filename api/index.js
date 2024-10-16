43a78b6a1ad34e53aea1f692c221760a12a9c53b
Febin <febinaugustine7@gmail.com>
2024-10-16 16:46:14 +0530

    Database added

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONODB_CONNECTION)
+  .then(() => {
+    console.log("Mongo DB is Connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
