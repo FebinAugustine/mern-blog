83eead653cfb0be450daac964d4154a0d8569d73
Febin <febinaugustine7@gmail.com>
2024-10-16 19:45:16 +0530

    middleware error added and also sdded a custom err handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   console.log(req.body);
 
   const { userName, email, password } = req.body;
@@ -15,7 +16,8 @@ export const signup = async (req, res) => {
     password === ""
   ) {
     console.log("All fields are required");
-    return res.status(404).json({ message: "All fields are required" });
+    // return res.status(404).json({ message: "All fields are required" });
+    next(errorHandler(400, "All fields are required"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -27,6 +29,6 @@ export const signup = async (req, res) => {
 
     res.json("SignUp Successful");
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
