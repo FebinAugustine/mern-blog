97bd921785cee891bea31bd58e443f9302adad90
Febin <febinaugustine7@gmail.com>
2024-10-20 00:56:00 +0530

    Added Redux Toolkit, store and slice and reducers. Next is Persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,24 +1,34 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice";
 // import axios from "axios";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  // const [errorMessage, setErrorMessage] = useState(null);
+  // const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!email || !password) {
-      return setErrorMessage("All fields are required.");
+      // return setErrorMessage("All fields are required.");
+      return dispatch(signInFailure("All fields are required."));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      // setLoading(true);
+      // setErrorMessage(null);
+      dispatch(signInStart());
 
       const res = await fetch(import.meta.env.VITE_BASE_URL2, {
         method: "POST",
@@ -27,16 +37,19 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        setLoading(false);
-        return setErrorMessage(data.message);
+        // setLoading(false);
+        // return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
-      if (data.success === true) {
-        navigate("/signin");
+      // setLoading(false);
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
       }
     } catch (error) {
-      setLoading(false);
-      setErrorMessage(error.message);
+      // setLoading(false);
+      // setErrorMessage(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
   return (
